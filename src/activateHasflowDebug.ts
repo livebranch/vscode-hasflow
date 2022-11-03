bcc9a7bb65774b19ceb484ad453a924b25c023d4
David Kemsley <david@livebranchstudios.com.au>
2022-11-04 09:32:20 +1100

    Move to esbuild (broken)

---
+++ src/activateHasflowDebug.ts
@@ -0,0 +1,216 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+/*
+ * activateHasflowDebug.ts containes the shared extension code that can be executed both in node.js and the browser.
+ */
+
+'use strict';
+
+import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken, window} from 'vscode';
+import { HasflowDebugSession } from './hasflowDebug';
+import { FileAccessor } from './hasflowRuntime';
+
+export function activateHasflowDebug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory) {
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.hasflow-debug.runEditorContents', (resource: vscode.Uri) => {
+			window.showInformationMessage("Called extension.hasflow-debug.runEditorContents");
+
+
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+					type: 'hasflow',
+					name: 'Run File',
+					request: 'launch',
+					program: targetResource.fsPath
+				},
+					{ noDebug: true }
+				);
+			}
+		}),
+		vscode.commands.registerCommand('extension.hasflow-debug.debugEditorContents', (resource: vscode.Uri) => {
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+					type: 'hasflow',
+					name: 'Debug File',
+					request: 'launch',
+					program: targetResource.fsPath,
+					stopOnEntry: true
+				});
+			}
+		}),
+	);
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.hasflow-debug.getProgramName', config => {
+		return vscode.window.showInputBox({
+			placeHolder: "Please enter the name of a markdown file in the workspace folder",
+			value: "readme.md"
+		});
+	}));
+
+	// register a configuration provider for 'hasflow' debug type
+	const provider = new HasflowConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('hasflow', provider));
+
+	// register a dynamic configuration provider for 'hasflow' debug type
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('hasflow', {
+		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+			return [
+				{
+					name: "Dynamic Launch",
+					request: "launch",
+					type: "hasflow",
+					program: "${file}"
+				},
+				{
+					name: "Another Dynamic Launch",
+					request: "launch",
+					type: "hasflow",
+					program: "${file}"
+				},
+				{
+					name: "Hasflow Launch",
+					request: "launch",
+					type: "hasflow",
+					program: "${file}"
+				}
+			];
+		}
+	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+
+	if (!factory) {
+		factory = new InlineDebugAdapterFactory();
+	}
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('hasflow', factory));
+	if ('dispose' in factory) {
+		context.subscriptions.push(factory);
+	}
+
+	// override VS Code's default implementation of the debug hover
+	// here we match only Hasflow "variables", that are words starting with an '$'
+	context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('yaml', {
+		provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
+
+			const VARIABLE_REGEXP = /\$[a-z][a-z0-9]*/ig;
+			const line = document.lineAt(position.line).text;
+
+			let m: RegExpExecArray | null;
+			while (m = VARIABLE_REGEXP.exec(line)) {
+				const varRange = new vscode.Range(position.line, m.index, position.line, m.index + m[0].length);
+
+				if (varRange.contains(position)) {
+					return new vscode.EvaluatableExpression(varRange);
+				}
+			}
+			return undefined;
+		}
+	}));
+
+	// override VS Code's default implementation of the "inline values" feature"
+	context.subscriptions.push(vscode.languages.registerInlineValuesProvider('yaml', {
+
+		provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
+
+			const allValues: vscode.InlineValue[] = [];
+
+			for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
+				const line = document.lineAt(l);
+				var regExp = /\$([a-z][a-z0-9]*)/ig;	// variables are words starting with '$'
+				do {
+					var m = regExp.exec(line.text);
+					if (m) {
+						const varName = m[1];
+						const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
+
+						// some literal text
+						//allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
+
+						// value found via variable lookup
+						allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
+
+						// value determined via expression evaluation
+						//allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
+					}
+				} while (m);
+			}
+
+			return allValues;
+		}
+	}));
+}
+
+class HasflowConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'yaml') {
+				config.type = 'hasflow';
+				config.name = 'Launch';
+				config.request = 'launch';
+				if (/* platform() == "win32" */false) {
+					config.debugger = 'hasflow.exe';
+				} else {
+					config.debugger = 'hasflow';
+				}
+				config.initialSeederTags = [];
+			}
+		}
+
+		// if (!config.program) {
+		// 	return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+		// 		return undefined;	// abort launch
+		// 	});
+		// }
+
+		return config;
+	}
+}
+
+export const workspaceFileAccessor: FileAccessor = {
+	isWindows: false,
+	async readFile(path: string): Promise<Uint8Array> {
+		let uri: vscode.Uri;
+		try {
+			uri = pathToUri(path);
+		} catch (e) {
+			return new TextEncoder().encode(`cannot read '${path}'`);
+		}
+
+		return await vscode.workspace.fs.readFile(uri);
+	},
+	async writeFile(path: string, contents: Uint8Array) {
+		await vscode.workspace.fs.writeFile(pathToUri(path), contents);
+	}
+};
+
+function pathToUri(path: string) {
+	try {
+		return vscode.Uri.file(path);
+	} catch (e) {
+		return vscode.Uri.parse(path);
+	}
+}
+
+class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
+		return new vscode.DebugAdapterInlineImplementation(new HasflowDebugSession(workspaceFileAccessor));
+	}
+}
